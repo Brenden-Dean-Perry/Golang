c864280c852ccd1409ebec15bfb6eb9dc13f39a5
Brenden Perry <brendenperry5@gmail.com>
2025-09-24 21:41:54 -0700

    Add switch statement examples to controlflow.go
    Added two new functions to demonstrate switch statements in Go: one for mapping days of the week and another for grading based on score. Also added a new file logicaloperators.go as a placeholder.

--- Code/controlflow.go
+++ Code/controlflow.go
@@ -10,3 +10,41 @@ func if_else_example(x int) string {
 		return "odd"
 	}
 }
+
+// Switch statement
+func switch_example(day int) string {
+	// No need for break statements; Go automatically breaks after each case
+	switch day {
+	case 1:
+		return "Monday"
+	case 2:
+		return "Tuesday"
+	case 3:
+		return "Wednesday"
+	case 4:
+		return "Thursday"
+	case 5:
+		return "Friday"
+	case 6:
+		return "Saturday"
+	case 7:
+		return "Sunday"
+	default:
+		return "Invalid day"
+	}
+}
+
+// Switch with multiple discrete conditions
+func conditional_switch_example(score int) string {
+	switch score {
+	case 90, 100:
+		return "A"
+	case 80, 89:
+		return "B"
+	case 70, 79:
+		return "C"
+	default:
+		return "F"
+	}
+
+}
